a5d2adb0bb283c36fa67bf225a92a9e341da94cb
jasper postema <jasperpostema@hotmail.nl>
2023-05-08 16:40:19 +0200

    Added albums and nicer view of the window

--- Window.py
+++ Window.py
@@ -1,28 +1,75 @@
 import wx
 import vlc
-import time
-
+import os
+import eyed3
+import numpy as np
 
 mp3_directory = "mp3"
 
-class Music_window():
+song_dict = {f:{"location":os.path.join(mp3_directory,f)} for f in os.listdir(mp3_directory) if os.path.isfile(os.path.join(mp3_directory,f))}
 
-    def __init__(self, parent, title):
-        self.frame = wx.Frame(parent, title=title,size=(900, 800))
+for i,song in enumerate(song_dict.keys()):
+    song_dict[song]["album"] = eyed3.load(song_dict[song]["location"]).tag.album
+    song_dict[song]["ID"] = i
 
-        self.panel = wx.Panel(self.frame, wx.ID_ANY)
-        self.playbutton=wx.Button(self.panel,wx.ID_ANY,"test",(10,10))
-        song = "vo.mp3"
-        media = vlc.MediaPlayer(mp3_directory+"/"+song)
-        self.playbutton.Bind(wx.EVT_BUTTON,self.Play_song(media))
+#All the different albums
+Albums_dict = {a:{"ID":i,"Occurences":[sd["album"]for sd in song_dict.values()].count(a)} for i,a in enumerate(np.unique([sd["album"]for sd in song_dict.values()]))}
 
-        self.frame.Centre()
+
+class Music_Panel(wx.Panel):
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self,parent)
+
+        self.playing_dict = {}
+        self.player_dict = {}
+        self.button_dict = {}
+
+        prev_occ = 0
+        for album,album_dict in Albums_dict.items():            
+            occ = album_dict["Occurences"]
+            
+            Albums_dict[album]["staticbox"] = wx.StaticBox(self,label=album,pos=(5,10+50*prev_occ),size=(550,50*(occ+0.5)))
+            prev_occ += occ+0.5
+            Albums_dict[album]["nrChildren"] = 0
+
+        for song in song_dict.keys():
+            ID = song_dict[song]["ID"]
+            album = song_dict[song]["album"]
+            i = Albums_dict[album]["nrChildren"] 
+            playbutton=wx.Button(Albums_dict[album]["staticbox"],ID,song,(15,50*i+30))
+            self.button_dict[ID] = playbutton
+
+            self.playing_dict[ID] = False
+            p = vlc.MediaPlayer(song_dict[song]["location"])
+            self.player_dict[ID] = p
+
+            self.Bind(wx.EVT_BUTTON,self.Play_song,playbutton)
+            Albums_dict[album]["nrChildren"] +=1
+
+        self.Center()
     
 
-    def Play_song(self,media):
-        media.play()
-        #time.sleep(15)
-        #media.stop()
+    def Play_song(self,event):
+        Id = event.GetId()
+        p =  self.player_dict[Id] 
+
+        song = [song for song,sd in song_dict.items() if sd["ID"]==Id][0]
+
+        if not self.playing_dict[Id]:
+            p.play()
+            button = self.button_dict[Id]
+            button.SetLabel("(playing) "+song)
+            button.SetSize((button.GetSize()[0]+60,button.GetSize()[1]))
+            self.playing_dict[Id] = True
+
+        else:
+            p.stop()
+            button = self.button_dict[Id]
+            button.SetLabel(song)
+            button.SetSize((button.GetSize()[0]-60,button.GetSize()[1]))
+            self.playing_dict[Id] = False
+
 
 
 
@@ -30,8 +77,9 @@ class Music_window():
 def main():
 
     app = wx.App()
-    Mw = Music_window(None, title='Music master+')
-    Mw.frame.Show()
+    frame = wx.Frame(None,size=(900, 800), title='Music master+')
+    Music_Panel(frame)
+    frame.Show()
     app.MainLoop()
 
 
